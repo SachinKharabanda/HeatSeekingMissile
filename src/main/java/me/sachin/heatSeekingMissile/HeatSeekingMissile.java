224c9335c172052be106dc26a60be71821fd95dc
Sachin <sachin.kharabanda@aol.com>
2025-09-12 10:58:16 +0100

    Initial commit

---
+++ src/main/java/me/sachin/heatSeekingMissile/HeatSeekingMissile.java
@@ -0,0 +1,17 @@
+package me.sachin.heatSeekingMissile;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HeatSeekingMissile extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
